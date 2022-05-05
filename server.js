91d0578d3004353dd43fa90605cdb0891849f8fe
Natalie Bulger <bulgern20@gmail.com>
2022-05-04 18:01:40 -0700

    Adding heroku port command

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const uuid = require("generate-unique-id");
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
