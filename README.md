1d59c6a131a663ac7ecd140c6f4aa102f596f46e
Natalie Bulger <bulgern20@gmail.com>
2022-05-06 14:21:47 -0700

    Updating README and commenting server.js file

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 
 ## Description
 
+I was provided starter code and tasked to create a note taker that a user can use to write and save notes. The application uses Express.js as a back-end and saves/retrieves notes from a JSON file.
+
+Once the front and back end were connected, I deployed the application to Heroku (see link at the bottom of README).
+
 ## Table of Contents
 
 - [User Story](#user-story)
 - [Acceptance Criteria](#acceptance-criteria)
+- [Node Packages](#node-packages)
+- [Middleware](#middleware)
+- [GET Requests](#get-requests)
+- [POST Request](#post-request)
+- [DELETE Request](#delete-request)
+- [Screen Recording](#screen-recording)
+- [Link](#link)
 
 ## User Story
 
@@ -32,3 +43,41 @@ THEN that note appears in the right-hand column
 WHEN I click on the Write icon in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 ```
+
+## Node Packages
+
+In order to build the server Javascript file, I started by requiring the necessary packages from the node package manager. I needed three different packages to completed the back-end application:
+
+1. Express
+2. fs
+3. generate-unique-id
+
+## Middleware
+
+Within the server file, I used three pieces of middleware:
+
+1. express.json
+2. express.urlencoded
+3. express.static
+
+The express.json middleware recognizes incoming data as a JSON object. The urlencoded middleware recognizes incoming data as a string or array. The express.static middleware tells the server to check if any of the static files in the public folder will work for the user request before moving on the GET, POST, and DELETE requests.
+
+## GET Requests
+
+I started out the requests by creating a GET request for the root directory that sends back the index.html file in the public folder. Then I created a GET request for the /notes portion of the page that sends back the notes.html file in the public folder. Finally, I created a GET request that reads the db.json file that holds all the saved notes, parses them, and returns them so the index.js file can populate the saved notes on the left side of the page.
+
+## POST Request
+
+The post request is called when the user clicks the save button after entering a note title and note text content. First the request console logs that the request has been received. Then it breaks up the request body into two seperate variables: title and text. If both title and text are present then the post request creates a new note with the title, text, and a unique id using the node package "generate-unique-id". From there the request reads the db.json file, parses the notes array, pushes the new note onto the array, and then re-writes the db.json files including the new notes.
+
+## DELETE Request
+
+Finally I created a delete request that is called when the client clicks on the trash can next to the notes that are saved on the left hand side of the page. First, the request parses the data from the db file. Then, the request creates an id variable using the id in the request parameter and generates a noteIndex that finds the index in the parsed Notes with the same id. Once the id that matches is found, the request splices that note out of the parsed notes array. Then it re-writes the db.json file without the deleted note and returns the new parsed notes so the index.js can repopulate the notes on the left side of the page.
+
+## Screen Recording
+
+![GIF demonstrating the functionality of the front-end application, adding and deleting notes, and the back-end server, updating the JSON file with the new or removed notes, running using express.js and deployed using Heroku]()
+
+## Link
+
+See the following for a link to my deployed application: https://salty-depths-00367.herokuapp.com/
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+// Require express, fs, and the generate-unique-id node packages
 const express = require("express");
 const fs = require("fs");
 const uuid = require("generate-unique-id");
+// Generate the listening port either for the heroku port or 3005
 const PORT = process.env.PORT || 3005;
 
+// Call the express application
 const app = express();
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,6 +23,7 @@ app.get("/notes", (req, res) =>
   res.sendFile("/public/notes.html", { root: __dirname })
 );
 
+// When in the notes directory
 app.get("/api/notes", (req, res) => {
   //read the db.json file and return the saved notes
   fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -31,68 +36,77 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
+// Post request called when client clicks on the save icon: save a new note to the db.json file and return the new note to the client's saved notes
 app.post("/api/notes", (req, res) => {
-  //save a new note to the db.json file and return the new note to the client
+  //console that the request was received
   console.info(`${req.method} request received to add a note`);
 
-  // Destructuring assignment for the items in req.body
+  // Destructure the items in req.body into note title and note text
   const { title, text } = req.body;
 
-  // If all the required properties are present
+  // If both title and text are present
   if (title && text) {
-    // Variable for the object we will save
+    // Variable for the object we will save with the unique id
     const newNote = {
       title,
       text,
       id: uuid(),
     };
 
-    // Obtain existing reviews
+    // Obtain existing notes by reading the db.json file
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
       } else {
         // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
-        // Add a new note
+        // Add a new note by pushing onto the parsed array
         parsedNotes.push(newNote);
-        // Write updated reviews back to the file
+        // Re-write the db.json file with the new parsed notes array
         fs.writeFile(
           "./db/db.json",
           JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info("Successfully updated notes!")
+              : //console out the the notes were successfully updated
+                console.info("Successfully updated notes!")
         );
       }
     });
 
+    //create a variable with the new note and success status
     const response = {
       status: "success",
       body: newNote,
     };
-
-    console.log(response);
+    //return that response as a 201 working type code
     res.status(201).json(response);
   } else {
-    res.status(500).json("Error in posting note");
+    //If there was an error then return that there was an error saving the note
+    res.status(500).json("Error in saving note");
   }
 });
 
+// When the user clicks the trash can icon next to a saved notes and call the delete request
 app.delete("/api/notes/:id", (req, res) => {
+  //read the db.json file
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
+      //parse out the array
       const parsedNotes = JSON.parse(data);
+      //create an id variable from the clicked note
       const { id } = req.params;
 
+      //create a note index that equals the index of the item in the array with the same id as the id from the clicked note
       const noteIndex = parsedNotes.findIndex((p) => p.id == id);
 
+      //splice that index item out of the parsed notes array
       parsedNotes.splice(noteIndex, 1);
 
-      //   res.send();
+      //re-write the db.json file without the deleted note
       fs.writeFile(
         "./db/db.json",
         JSON.stringify(parsedNotes, null, 4),
@@ -101,11 +115,13 @@ app.delete("/api/notes/:id", (req, res) => {
             ? console.error(writeErr)
             : console.info("Successfully updated notes!")
       );
+      //return the new parsed notes array to repopulate the saved notes on the left-hand side of the page
       return res.json(parsedNotes);
     }
   });
 });
 
+//Tell the application to listen at the defined port variable
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
